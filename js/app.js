a36dee7d2bd77b10f6673fc102519e2360525b40
sajim-khan <shossain1427@gmail.com>
2023-02-23 17:28:57 +0600

    added more load data

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function loadData2 (){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
+
+
+function loadUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => display(json))
+}
+
+function display(json){
+    console.log(json);
+}
